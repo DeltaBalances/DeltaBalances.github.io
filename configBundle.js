7c607f64dbbfbab041aabe757c731305efa642fb
DeltaBalances <30391324+DeltaBalances@users.noreply.github.com>
2019-01-01 04:51:23 +0100

    Dec 2018 block range

--- configBundle.js
+++ configBundle.js
@@ -559,6 +559,7 @@ module.exports = {
     { m: "Sept 2018", blockFrom: 6249399, blockTo: 6430273 },
     { m: "Oct 2018", blockFrom: 6430273, blockTo: 6620476 },
     { m: "Nov 2018", blockFrom: 6620476, blockTo: 6803256 },
+    { m: "Dec 2018", blockFrom: 6803256, blockTo: 6988615 },
   ],
 
   history: {

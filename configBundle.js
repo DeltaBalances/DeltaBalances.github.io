c3daed56cacc5b64043352cbfac49115d2f12cd9
DeltaBalances <30391324+DeltaBalances@users.noreply.github.com>
2019-06-30 18:29:51 +0200

    Add cloudflare web3 provider

--- configBundle.js
+++ configBundle.js
@@ -69,8 +69,8 @@ module.exports = {
   infuraURL: 'https://mainnet.infura.io/v3/1b000043e1a84c468747a6b75b4541c1',  // infura project for ethereum logs (history)
   web3URLs: {
     Infura: 'https://mainnet.infura.io/v3/a736fb84e7d74effb636f41305a0afd3', // infura project for web3 calls
-    //myEtherApi: 'https://api.myetherapi.com/eth',
-    //myCryptoApi: 'https://api.mycryptoapi.com/eth',
+    Cloudflare: 'https://cloudflare-eth.com',
+    myCrypto: 'https://api.mycryptoapi.com/eth',
   },
   localNode: 'http://localhost:8545',
   socketURL: 'https://api.forkdelta.com', //forkdelta order/price api
